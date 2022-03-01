5df43215d4c011c374f1d02755b1aee395169b70
Garrett <garres4@vt.edu>
2022-03-01 16:46:53 -0700

    initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Nav, NavbarContainer } from './Navbar.elements'
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">
+                    <NavIcon />
+                </NavLogo>
+            </NavbarContainer>
+        </Nav>
+    </>
+  )
+}
+
+export default Navbar
