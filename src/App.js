0062747621d3507d62d5bd52be82fefb84ee6618
Garrett <garres4@vt.edu>
2022-03-02 11:30:23 -0700

    finished navbar except for mobile menu close responsive navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {Navbar} from './components'
 import GlobalStyle from './globalStyles';
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/HomePage/Home';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+      </Routes>
     </>
   );
 }
---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    InfoSec
+} from './InfoSection.elements'
+
+const InfoSection = ({lightBg}) => {
+  return (
+    <>
+       <InfoSec lightBg={lightBg}>
+           Hi
+        </InfoSec> 
+    </>
+  )
+}
+
+export default InfoSection
